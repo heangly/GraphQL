15b9c342ec33d64ccc16feb6bab8caff7ec12b7d
Heang Ly <heanglypa@gmail.com>
2022-05-26 13:25:51 -0400

    add query

---
+++ GraphiQL-App/users/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema
+  })
+)
+
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log(`==> Server is running on PORT ${PORT} <==`)
+})
